e5edeefb6dc6496d233a5a8ee6855e442637badc
sidshrivastav <siddhantshrivastava@outlook.com>
2024-01-26 20:12:14 +0530

    feat: day 02 part 2 solution added

--- day-02/main.go
+++ day-02/main.go
@@ -48,10 +48,49 @@ func isConfigurationPossible(game string) int {
 
 }
 
+func intMax(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func cubeSetPower(game string) int {
+	red := 0 
+	green := 0
+	blue := 0
+	gameData := strings.Split(strings.Split(game, ":")[1], ";")
+	for _, gameSet :=  range gameData {
+		for _, gameCubes := range strings.Split(gameSet, ",") {
+			gameCube := strings.Split(gameCubes, " ")
+			cubeCount, err := strconv.Atoi(gameCube[1])
+			if err != nil {
+				panic(err)
+			}
+			if gameCube[2] == "red" {
+				red = intMax(cubeCount, red)
+			}
+			if gameCube[2] == "green" {
+				green = intMax(cubeCount, green)
+			}
+			if gameCube[2] == "blue" {
+				blue = intMax(cubeCount, blue)
+			}
+		}
+	}
+	
+
+	return red * blue * green
+}
+
+
 func solve(part string, gameRound []string) (result int) {
 	for _, value := range gameRound {
 		if part == "1" && value != "" {
 			result += isConfigurationPossible(value)
+		}
+		if part == "2" && value != "" {
+			result += cubeSetPower(value)
 		}
 	}
 
@@ -66,5 +105,15 @@ func main() {
 		panic(err)
 	}
 	gameRound := strings.Split(string(content), "\n")
-	fmt.Println(solve("1", gameRound))
+	fmt.Println("Part 1: ", solve("1", gameRound))
+
+	// Part 1
+	inputFile = "input_2.txt"
+	content, err = os.ReadFile(inputFile)
+	if err != nil {
+		panic(err)
+	}
+	gameRound = strings.Split(string(content), "\n")
+	fmt.Println("Part 2: ", solve("2", gameRound))
+
 }
